88c51491fb4f423c1d0bda19321a4fe618846dc5
realm_bmc <realmbmc@mail.ru>
2018-11-26 22:51:39 +0300

    removed session_id update

--- mutliadmin/MultiAdmin/Server.cs
+++ mutliadmin/MultiAdmin/Server.cs
@@ -288,7 +288,7 @@ namespace MultiAdmin.MultiAdmin
 			if (ServerModCheck(1, 5, 0))
 			{
 				SendMessage("RECONNECTRS");
-				session_id = Utils.GetUnixTime().ToString();
+				//session_id = Utils.GetUnixTime().ToString();
 			}
 			else
 				gameProcess.Kill();
